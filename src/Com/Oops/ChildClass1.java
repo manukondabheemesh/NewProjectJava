c3af509f973df8a16acaf81c49107e256ef7e446
bheemeswararao <demo@gmail.com>
2019-06-30 00:16:16 +0530

    Added new classes

---
+++ src/Com/Oops/ChildClass1.java
@@ -0,0 +1,14 @@
+package Com.Oops;
+
+public class ChildClass1 extends BaseClassJava {
+
+	public static void main(String[] args) {
+		
+		ChildClass1 ChildClass1ref=new ChildClass1();
+		ChildClass1ref.defaultmethod();
+		ChildClass1ref.protectedmethod();
+		ChildClass1ref.publicmethod();
+		
+	}
+
+}
